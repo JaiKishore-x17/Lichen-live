a8077971d51989f0edf0b3cd398b9a23f09d2523
jaisai1709-afk <jaisai1709@gmail.com>
2026-03-04 18:35:38 +0530

    Ver.0.0.2c Coded - security added.

--- LichenApp/src/firebase/config.js
+++ LichenApp/src/firebase/config.js
@@ -3,13 +3,13 @@
 // but we keep the config for future SDK use or other services.
 
 export const firebaseConfig = {
-    apiKey: "AIzaSyDzVWwlmAJjP6JN7hoqMmQFQpyLq0gSpxc",
-    authDomain: "lichen-live.firebaseapp.com",
-    databaseURL: "https://lichen-live-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "lichen-live",
-    storageBucket: "lichen-live.firebasestorage.app",
-    messagingSenderId: "794470653949",
-    appId: "1:794470653949:web:20bbaf98b0e9aaf13cc394"
+    apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.EXPO_PUBLIC_FIREBASE_DATABASE_URL,
+    projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID
 };
 
 export const REST_DB_URL = firebaseConfig.databaseURL;
